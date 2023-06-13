c2f71df051fd99bd1b12073643d24a7ebb434175
caiomaestrello <105826879+caiomaestrello@users.noreply.github.com>
2023-06-13 19:14:18 -0300

    Add files via upload

---
+++ Snake Game/index.js
@@ -0,0 +1,198 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+class SnakePart {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+//velocidade inicial
+let speed = 5;
+
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+
+let headX = 10;
+let headY = 10;
+const snakeParts = [];
+let tailLength = 2;
+
+let appleX = 5;
+let appleY = 5;
+
+let inputsXVelocity = 0;
+let inputsYVelocity = 0;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+let score = 0;
+
+function drawGame() {
+  xVelocity = inputsXVelocity;
+  yVelocity = inputsYVelocity;
+
+  changeSnakePosition();
+  let result = isGameOver();
+  if (result) {
+    return;
+  }
+
+  clearScreen();
+
+  checkAppleCollision();
+  drawApple();
+  drawSnake();
+
+  drawScore();
+
+  if (score > 5) {
+    speed = 9;
+  }
+  if (score > 10) {
+    speed = 11;
+  }
+
+  setTimeout(drawGame, 1000 / speed);
+}
+
+//funcao para encerrar o jogo
+function isGameOver() {
+  let gameOver = false;
+
+  //mudancas de velocidade com pontuacao
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  if (headX < 0) {
+    gameOver = true;
+  } else if (headX === tileCount) {
+    gameOver = true;
+  } else if (headY < 0) {
+    gameOver = true;
+  } else if (headY === tileCount) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      gameOver = true;
+      break;
+    }
+  }
+
+  if (gameOver) {
+    ctx.fillStyle = "white";
+    ctx.font = "50px Verdana";
+
+    if (gameOver) {
+      ctx.fillStyle = "white";
+      ctx.font = "50px Verdana";
+
+      var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+      gradient.addColorStop("0", " magenta");
+      gradient.addColorStop("0.5", "blue");
+      gradient.addColorStop("1.0", "red");
+
+      ctx.fillStyle = gradient;
+
+      ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+    }
+
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+  }
+
+  return gameOver;
+}
+
+//cor do placar
+function drawScore() {
+  ctx.fillStyle = "pink";
+  ctx.font = "10px Verdana";
+  ctx.fillText("Score " + score, canvas.width - 60, 10);
+}
+
+//fundo jogo
+function clearScreen() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+//corpo
+function drawSnake() {
+  ctx.fillStyle = "green";
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
+
+  //aumentar o tamanho
+  snakeParts.push(new SnakePart(headX, headY));
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift();
+  }
+
+//cabeca
+  ctx.fillStyle = "blue";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition() {
+  headX = headX + xVelocity;
+  headY = headY + yVelocity;
+}
+
+//maca
+function drawApple() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+}
+
+//colissao com a maca
+function checkAppleCollision() {
+  if (appleX === headX && appleY == headY) {
+    appleX = Math.floor(Math.random() * tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    score++;
+  }
+}
+
+document.body.addEventListener("keydown", keyDown);
+//teclas setas
+function keyDown(event) {
+
+  //cima
+  if (event.keyCode == 38 || event.keyCode == 87) {
+    if (inputsYVelocity == 1) return;
+    inputsYVelocity = -1;
+    inputsXVelocity = 0;
+  }
+
+  //baixo
+  if (event.keyCode == 40 || event.keyCode == 83) {
+    if (inputsYVelocity == -1) return;
+    inputsYVelocity = 1;
+    inputsXVelocity = 0;
+  }
+
+  //esquerda
+  if (event.keyCode == 37 || event.keyCode == 65) {
+    if (inputsXVelocity == 1) return;
+    inputsYVelocity = 0;
+    inputsXVelocity = -1;
+  }
+
+  //direita
+  if (event.keyCode == 39 || event.keyCode == 68) {
+    if (inputsXVelocity == -1) return;
+    inputsYVelocity = 0;
+    inputsXVelocity = 1;
+  }
+}
+
+drawGame();
